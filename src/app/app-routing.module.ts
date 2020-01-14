9217512c773ff6dfaffabf365d5cd09dd8493df5
GiridharaMG <mg.giridhar@gmail.com>
2020-01-14 11:26:52 +0530

    Class Project 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { SearchProductComponent } from './search-product/search-product.component';
+import { DataFromJsonComponent } from './data-from-json/data-from-json.component';
+import { SearchOneComponent } from './search-one/search-one.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+import { DeleteComponent } from './delete/delete.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'add', component: AddProductComponent, canActivate: [AuthGuard], data: { expected: 'giridhar@ty.com' } },
+  { path: 'search', component: SearchProductComponent },
+  { path: 'delete/:id', component: DeleteComponent, canActivate: [AuthGuard], data: { expected: 'giridhar@ty.com' } },
+  { path: 'data-from-json', component: DataFromJsonComponent },
+  { path: 'search-one', component: SearchOneComponent },
+  { path: 'edit/:id', component: EditProductComponent, canActivate: [AuthGuard], data: { expected: 'giridhar@ty.com' } }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrudModule } from './crud.module';
+import { LoginModule } from './login.module';
+import { ExtraModule } from './extra.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CrudModule,
+    LoginModule,
+    ExtraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
